f2af553e903e5cbda5d0ee41e70ef8f2423b7513
GTFalcao <gtfalcao96@gmail.com>
2019-08-25 23:53:24 -0300

    Master - Production Build

---
+++ README.md
@@ -0,0 +1,14 @@
+### Teste de Front-End Wooza
+------------
+
+Para iniciar o aplicativo, basta abrir o arquivo **index.html** na pasta **build**.
+
+O aplicativo foi feito em **React.js**. O código-fonte está na pasta **src**.
+
+Conforme orientação do teste, ao contratar um plano, os dados relevantes serão impressos no console do navegador.
+
+------------
+**Guilherme Falcão**
+https://github.com/GTFalcao
+gtfalcao96@gmail.com
+(21) 99689-7044
---
+++ build/precache-manifest.2295a59fad01ccbaaf2e6bb1a8af9783.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "633ff30a4acff9652e805a8042dda10c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "439d06a1caf4d2a046a9",
+    "url": "/static/css/main.9e318293.chunk.css"
+  },
+  {
+    "revision": "4daf7d0d98d4c77aa342",
+    "url": "/static/js/2.7906b74d.chunk.js"
+  },
+  {
+    "revision": "439d06a1caf4d2a046a9",
+    "url": "/static/js/main.29d59d20.chunk.js"
+  },
+  {
+    "revision": "6617796e1b4c8366c2a8",
+    "url": "/static/js/runtime~main.1848c660.js"
+  }
+]);
